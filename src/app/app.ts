86b938b70aec8a8fa98a1996fe7a6871d068dac4
DIR\marvin.pacot <marvin.pacot@accenture.com>
2025-10-23 00:06:55 +0800

    Component Discovery Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, Home, Header],
+  template: `
+             <p>this is rendered in root<p>
+             <app-header/>
+             <app-home/>`,
   styleUrl: './app.scss'
 })
 export class App {
